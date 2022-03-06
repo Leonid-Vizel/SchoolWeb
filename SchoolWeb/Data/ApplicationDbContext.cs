c97f7e7a02b42f93f850fb2228f9a9eeb4cde3ec
Leonid-Vizel <spartanlaska@gmail.com>
2022-03-06 18:52:14 +0300

    Подготовка к базе

---
+++ SchoolWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolWeb.Models;
+
+namespace SchoolWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<EgeResult> egeResults;
+        public DbSet<OgeResult> ogeResults;
+    }
+}
