b6b23bb41cb8544f02336df6bed5f79cceee3371
Vizel Leonid <spartanlaska@gmail.com>
2022-05-08 16:04:54 +0300

    [Update] >Добавлен контроллер Gallery >Добавлен контроллер Document >Начато тестирование расширения изображения на весь экран

---
+++ SchoolWeb/Controllers/DocumentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolWeb.Controllers
+{
+    public class DocumentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
