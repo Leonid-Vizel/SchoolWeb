90eaa6aaf7963f260028a397624b00372e83298b
Vizel Leonid <spartanlaska@gmail.com>
2022-05-14 15:32:13 +0300

    [Image update] >Настроено отображение картинки во вкладке здорового питания >Добавлено расширение картинок на весь экран при нажатии на них на страницах кроме галереи (#9)

---
+++ SchoolWeb/wwwroot/js/fullscreen-id.js
@@ -0,0 +1,17 @@
+﻿function makeFullScreen(id) {
+    var divObj = document.getElementById("image-" + id);
+    if (divObj.requestFullscreen) {
+        divObj.requestFullscreen();
+    }
+    else if (divObj.msRequestFullscreen) {
+        divObj.msRequestFullscreen();
+    }
+    else if (divObj.mozRequestFullScreen) {
+        divObj.mozRequestFullScreen();
+    }
+    else if (divObj.webkitRequestFullscreen) {
+        divObj.webkitRequestFullscreen();
+    } else {
+        window.open(divObj.getAttribute("src"), '_blank').focus();
+    }
+}
